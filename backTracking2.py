2272bde7bf8f7f33a5f6eb985cc4016f961bb33e
Bo-PC <b26wei@uwaterloo.ca>
2019-12-27 19:34:06 -0500

    backTracking2 added

--- backTracking2.py
+++ backTracking2.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 
 def solve(b):
     return solve_rec(b, 81 - 1)
@@ -10,20 +8,21 @@ def solve_rec(b, square):
         # checked last square
         return b
 
-    b = deepcopy(b)
     row = (square // 9)
     col = (square % 9)
     if b[row][col] != 0:
         # square value is not 0
         return solve_rec(b, square - 1)
 
-    test = deepcopy(b)
     for guess in range(9):
-        test[row][col] = guess + 1
-        if valid_board(test):
-            rec_b = solve_rec(test, square - 1)
-            if complete_board(rec_b) and valid_board(rec_b):
+        b[row][col] = guess + 1
+        pretty_print_board(b)
+        if valid_move(b, row, col):
+            rec_b = solve_rec(b, square - 1)
+            if complete_board(rec_b) and valid_move(rec_b, row, col):
                 return rec_b
+
+    b[row][col] = 0
     return b
 
 
@@ -42,20 +41,44 @@ def valid_board(b):
     return True
 
 
+def valid_move(b, row, col):
+    if not valid_group(b[row]) or not valid_group([b[row][col] for row in range(9)]):
+        return False
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    if not valid_group([b[row][col] for row in range(start_row, start_row + 3) for col in range(start_col, start_col + 3)]):
+        return False
+    return True
+
+
 def valid_group(g):
-    return all(g.count(i+1) <= 1 for i in range(9))
+    seen = []
+    for i in g:
+        if i != 0 and i in seen:
+            return False
+        else:
+            seen.append(i)
+    return True
 
 
 def complete_board(b):
     return all(s != 0 for row in b for s in row)
 
 
-def format_board(file_name):
-    board_file = open("boards/" + file_name + ".txt", "r")
+def format_board(fn):
+    board_file = open("boards/" + fn + ".txt", "r")
     return list(filter(lambda x: len(x) != 0, [list(map(int, filter(lambda x: x != '\n', line.split(" ")))) for line in board_file]))
 
 
-file_name = input("File:")
-board = format_board(file_name)
-print(board)
-print(solve(board))
+def pretty_print_board(b):
+    for row in b:
+        for num in row:
+            print(num, end='')
+        print()
+    print()
+
+
+board = format_board(input("File:"))
+
+pretty_print_board(board)
+pretty_print_board(solve(board))
